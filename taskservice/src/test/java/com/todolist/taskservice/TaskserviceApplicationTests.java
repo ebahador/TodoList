4786fdc87c45fe7f5b25b8c3c05deaf12f74949d
Amir Bahador Eizadkhah <eizadkhah.bit@gmail.com>
2024-09-10 19:37:08 -0400

    Add two microservices as separate modules: userservice and taskservice

---
+++ taskservice/src/test/java/com/todolist/taskservice/TaskserviceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.todolist.taskservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskserviceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
